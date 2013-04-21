0bb942732d7a836f72cb3afa648ec146702c8e60
Denys Telezhkin <virusvt@MacBook-Air-Denys.local>
2013-04-21 17:28:21 +0300

    add docs for UIView+Loading

--- DTTableViewManager/UIView+Loading.h
+++ DTTableViewManager/UIView+Loading.h
@@ -8,7 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ `UIView+Loading` is a category on UIView, allowing to load view from .xib file.
+ 
+ */
+
 @interface UIView (Loading)
+
+/**
+ Loads view object from <xibName>.xib file.
+ 
+ @param xibName Name of the xib file to load from
+ 
+ @return view object
+ */
+
 + (id) loadFromXibNamed:(NSString *) xibName;
+
+/**
+ Loads view from xib with name identical to name of the current view class
+ 
+ @return view object
+ */
+
 + (id) loadFromXib;
 @end
