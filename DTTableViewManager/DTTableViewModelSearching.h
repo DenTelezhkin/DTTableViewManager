ca01945c7ae5e16544d41876709e3502aa345e03
Denys Telezhkin <strangervir@gmail.com>
2013-06-25 00:06:38 +0300

    add description for DTTableViewModelSearching protocol

--- DTTableViewManager/DTTableViewModelSearching.h
+++ DTTableViewManager/DTTableViewModelSearching.h
@@ -8,12 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
-@protocol DTTableViewModelSearching 
+/**
+ `DTTableViewModelSearching` protocol is used to search within UITableView models. If model does not implement methods of this protocol, DTTableViewManager will not add model to search results. Methods of this protocol are invoked for every model every time search occurs.
+ */
 
+
+@protocol DTTableViewModelSearching
 
 /**
  
  Use this method to filter models, that should show up, if search string is equal to searchString
+ 
+ @param searchString search string
+ 
+ @param scope current search scope
  */
 
 - (BOOL)shouldShowInSearchResultsForSearchString:(NSString*)searchString
