520d4d9b36720e6715f240d319dd62e7773e6da6
Denys Telezhkin <strangervir@gmail.com>
2013-10-24 11:16:02 +0300

    update licenses

--- DTTableViewManager/DTTableViewController.m
+++ DTTableViewManager/DTTableViewController.m
@@ -1,8 +1,8 @@
 //
 //  DTTableViewController.m
 //
-//  Created by Denys Telezhkin on 6/19/12.
-//  Copyright (c) 2012 MLSDev. All rights reserved.
+//  Created by Denys Telezhkin on 10/24/13.
+//  Copyright (c) 2013 MLSDev. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
