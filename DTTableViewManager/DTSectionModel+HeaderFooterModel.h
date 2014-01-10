b1228d8a5caea2667b40618e84de2b91f58b92a8
Denys Telezhkin <strangervir@gmail.com>
2014-01-10 17:18:42 +0200

    add docs for header footer category

--- DTTableViewManager/DTSectionModel+HeaderFooterModel.h
+++ DTTableViewManager/DTSectionModel+HeaderFooterModel.h
@@ -11,12 +11,38 @@
 static NSString * const DTTableViewElementSectionHeader = @"DTTableViewElementSectionHeader";
 static NSString * const DTTableViewElementSectionFooter = @"DTTableViewElementSectionFooter";
 
+/**
+ This category adds ability to set and get section footer and header model for current section.
+ */
+
 @interface DTSectionModel (HeaderFooterModel)
 
+/**
+ Set header model for current section. Header presentation depends on `DTTableViewController` sectionHeaderStyle property.
+ 
+@param headerModel headerModel for current section
+ */
 -(void)setHeaderModel:(id)headerModel;
+
+/**
+ Footer model for current section. Footer presentation depends on `DTTableViewController` sectionFooterStyle property.
+ 
+ @param footerModel footer model for current section
+ */
 -(void)setFooterModel:(id)footerModel;
 
+/**
+ Header model for current section.
+ 
+ @return headerModel
+ */
 -(id)headerModel;
+
+/**
+ Footer model for current section.
+ 
+ @return footerModel
+ */
 -(id)footerModel;
 
 @end
