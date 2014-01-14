7461296db35734b9c8f9a76b0e368b0760a9b02b
Denys Telezhkin <strangervir@gmail.com>
2014-01-14 13:21:33 +0200

    add documentation for default models

--- DTTableViewManager/DTDefaultCellModel.h
+++ DTTableViewManager/DTDefaultCellModel.h
@@ -5,16 +5,40 @@
 //  Created by Denys Telezhkin on 11.01.14.
 //  Copyright (c) 2014 Denys Telezhkin. All rights reserved.
 //
+
 #import "DTViewConfiguration.h"
 
+/**
+ `DTDefaultCellModel` is a custom model class, that allows to use UITableViewCells without subclassing.
+ */
 @interface DTDefaultCellModel : NSObject
 
+/**
+ Reuse identifier for cell, that will be used for current cell model.
+ */
 @property (nonatomic, retain) NSString * reuseIdentifier;
 
+/**
+ Cell style for cell, that will be created for current cell model
+ */
 @property (nonatomic, assign) UITableViewCellStyle cellStyle;
 
+/**
+ Configuration block, that will be executed on UITableViewCell after it will be created/reused.
+ */
 @property (nonatomic, copy) DTCellConfigurationBlock cellConfigurationBlock;
 
+/**
+ Convenience method, allowing to create `DTDefaultCellModel` instance.
+ 
+ @param style UITableViewCellStyle to use for cell
+ 
+ @param reuseIdentifier reuse identifier to use for cell
+ 
+ @param configurationBlock block to execute when cell is created
+ 
+ @return `DTDefaultCellModel` instance.
+ */
 +(instancetype)modelWithCellStyle:(UITableViewCellStyle)style
                   reuseIdentifier:(NSString *)reuseIdentifier
                configurationBlock:(DTCellConfigurationBlock)configurationBlock;
--- DTTableViewManager/DTDefaultHeaderFooterModel.h
+++ DTTableViewManager/DTDefaultHeaderFooterModel.h
@@ -8,12 +8,30 @@
 
 #import "DTViewConfiguration.h"
 
+/**
+ `DTDefaultHeaderFooterModel` is a custom model class, that allows to use UITableViewHeaderFooterView without subclassing.
+ */
 @interface DTDefaultHeaderFooterModel : NSObject
 
+/**
+ Reuse identifier for header footer view, that will be used for current model.
+ */
 @property (nonatomic, retain) NSString * reuseIdentifier;
 
+/**
+ Configuration block, that will be executed on UITableViewHeaderFooterView after it will be created/reused.
+ */
 @property (nonatomic, copy) DTViewConfigurationBlock viewConfigurationBlock;
 
+/**
+ Convenience method, allowing to create `DTDefaultHeaderFooterModel` instance.
+ 
+ @param reuseIdentifier reuse identifier to use for header footer view
+ 
+ @param configurationBlock block to execute when header footer view is created
+ 
+ @return `DTDefaultHeaderFooterModel` instance.
+ */
 +(instancetype)modelWithReuseIdentifier:(NSString *)reuseIdentifier
                      configurationBlock:(DTViewConfigurationBlock)configurationBlock;
 
