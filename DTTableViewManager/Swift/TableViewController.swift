32a2a166885ed8bc6629d9176a3ee733e90362ce
Denys Telezhkin <strangervir@gmail.com>
2015-07-12 14:22:20 +0300

    Restructure project into 2 separate parts - ObjectiveC and Swift

---
+++ DTTableViewManager/Swift/TableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewController.swift
+//  DTTableViewManager
+//
+//  Created by Denys Telezhkin on 12.07.15.
+//  Copyright (c) 2015 Denys Telezhkin. All rights reserved.
+//
+
+import UIKit
+import DTModelStorage
+
+class TableViewController: UIViewController {
+
+}
